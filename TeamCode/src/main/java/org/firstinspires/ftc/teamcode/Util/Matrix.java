a9f666206a0cea6812edabe9bf5408de15440e68
ethan123 <ethantt2012@gmail.com>
2020-02-09 21:40:54 -0800

    weeeee

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Matrix.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Matrix.java
@@ -18,7 +18,7 @@ public class Matrix {
         return res;
     }
 
-    public double[][] subtract(double[][] a, double [][] b){
+    public double[][] subtracto(double[][] a, double [][] b){
         int r1 = a.length;
         int c1 = a[0].length;
         int r2 = b.length;
