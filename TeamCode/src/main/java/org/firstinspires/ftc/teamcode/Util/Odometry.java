bede4cb9f40c9298dfabd626e9915fc71dbd4cc1
Null-Pointer-Exception <53492399+Null-Pointer-Exception@users.noreply.github.com>
2020-02-13 20:29:36 -0800

    Update Odometry.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Odometry.java
@@ -4,15 +4,15 @@ public class Odometry {
     final static double y0 = 1.0;
     final static double x0 = 1.0;
     final static double RADIUS = 1;
-    //[angle1, angle2, angle3]
-    public static int[] getPosition(double[] angles){
+    //[angle1, angle2, angle3][0]
+    public static int[] getPosition(double[][] angles){
         double[][] A = new double[][]{
             {y0, y0, 0.0},
             {x0, -x0, 2 * y0},
             {-1, 1, 0}
         };
         
-        double[] result = Matrix.
+        double[] result = Matrix(Matrix.cMult((Radius / (2 * y0), A)), angles);
         return result;
     }
 }
